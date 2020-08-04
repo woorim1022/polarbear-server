a12bd09118536184693f3a78c28c9be686883270
woorim1022 <woorim1022@naver.com>
2020-08-05 02:08:17 +0900

    weight.php

---
+++ weight.php
@@ -0,0 +1,27 @@
+<?php 
+    $link=mysqli_connect("localhost", "polarbear1022", "rladnfla7733!", "polarbear1022"); 
+
+    $uid = $_POST["uid"];
+	$wdate = date("Y-m-d", time());
+
+    $sql = "SELECT * FROM weights WHERE wid='test' AND wdate='$wdate'";
+    $result = mysqli_query($link, $sql);
+
+
+    $response = array();
+    
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)) {
+            $response["weight"]=$row["weight"];
+        }
+    }
+    else{
+        echo "MySQL failed!<br/>"; 
+    }
+
+   
+    echo json_encode($response);
+
+
+
+?>
