1bf3866cdfd6978d69f464aed334febda2f02a60
yeseonkim <66737920+yeseonkim@users.noreply.github.com>
2020-08-06 15:34:06 +0900

    Update mypage.php

---
+++ mypage.php
@@ -0,0 +1,29 @@
+<?php
+
+	$link=mysqli_connect("localhost", "polarbear1022", "rladnfla7733!", "polarbear1022");
+	mysqli_set_charset($link,"utf8");
+	
+	$uid=$_POST["uid"];
+	
+	$sql="SELECT SUM(apple) AS total1, SUM(fish) AS total2, SUM(meat) AS total3
+		   SUM(ice) AS total4 FROM buying_list WHERE uid='$uid' && uid=mypage_id GROUP BY mypage_id";
+	$result= mysqli_query($link, $sql);	  
+	
+	$response =array():
+	
+	if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)) {
+            $response["total1"]=$row["total1"];
+            $response["total2"]=$row["total2"];
+            $response["total3"]=$row["total3"];
+            $response["total4"]=$row["total4"];
+        }
+    }
+    else{
+        echo "MySQL failed!<br/>"; 
+    }
+
+   
+    echo json_encode($response);
+
+?>
